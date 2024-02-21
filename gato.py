5a0f279df8a9eff907eadaf343949ee062756d31
Aldo GOnzalez Vazquez <bytesroboticmx@gmail.com>
2024-02-20 19:34:29 -0600

    Agrega comentarios principales

--- gato.py
+++ gato.py
@@ -1,3 +1,14 @@
+#Descripcion: Juego del gato añadiendo un oponente con IA
+#Autor: Dr. Aldo Gonzalez Vazquez
+#Version: 1.0
+#Fecha: 20/0272024
+#Completar las siguientes funciones en el juego:
+#Dibujar las líneas del tablero: Implementar la función draw_lines para dibujar las líneas del juego en la pantalla.
+#Comprobar si un jugador ha ganado: Completar la función check_win para determinar si un jugador ha ganado el juego.
+#Comprobar si el juego termina en empate: Implementar la función check_draw para verificar si todos los cuadros están marcados y no hay ganador, lo que resulta en un empate.
+#Actualizar el estado del juego después de cada turno: Dibujar las marcas de los jugadores en el tablero y actualizar la interfaz gráfica para reflejar el estado actual del juego.
+#Finalizar el juego y determinar el ganador o empate: Mostrar un mensaje cuando el juego termine, ya sea por un ganador o por un empate.
+
 import pygame
 import sys
 
